9c565ba458b28e6d9d564f55c9979dbd77309ba0
nely de faria <nelly.nf54@gmail.com>
2022-08-06 14:22:14 -0300

    Cria atributo conta + método transferir

--- js-poo-1/ContaCorrente.js
+++ js-poo-1/ContaCorrente.js
@@ -1,5 +1,7 @@
 export class ContaCorrente {
     agencia;
+    cliente;
+
     _saldo = 0;
 
     sacar(valor) {
@@ -12,5 +14,10 @@ export class ContaCorrente {
     depositar(valor) {
         if (valor <= 0) return;
         this._saldo += valor;
+    }
+
+    transferir(valor, conta) {
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
     }
 }
