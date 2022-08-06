d14390c3ce233434323f2ebd0d5535e97043da25
nely de faria <nelly.nf54@gmail.com>
2022-08-06 14:05:10 -0300

    Organizando código

--- js-poo-1/index.js
+++ js-poo-1/index.js
@@ -1,24 +1,5 @@
-class Cliente {
-    nome;
-    cpf;
-}
-
-class ContaCorrente {
-    agencia;
-    _saldo = 0;
-
-    sacar(valor) {
-        if (this._saldo >= valor) {
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor) {
-        if (valor <= 0) return;
-        this._saldo += valor;
-    }
-}
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
 
 const cliente1 = new Cliente();
 
@@ -28,6 +9,15 @@ cliente1.cpf = 64062389070;
 const cliente2 = new Cliente();
 cliente2.nome = 'Alice';
 cliente2.cpf = 54717228090;
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(-100);
+contaCorrenteRicardo.sacar(50);
+console.log(contaCorrenteRicardo._saldo);
+console.log(contaCorrenteRicardo);
 
 console.log(cliente1);
 console.log(cliente2);
