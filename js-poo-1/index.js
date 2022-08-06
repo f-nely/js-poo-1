f855caeee1daecd5b05cc5cefed8e86053b04027
nely de faria <nelly.nf54@gmail.com>
2022-08-06 13:21:13 -0300

    Cria classe ContaCorrente

--- js-poo-1/index.js
+++ js-poo-1/index.js
@@ -1,21 +1,21 @@
 class Cliente {
     nome;
     cpf;
+}
+
+class ContaCorrente {
     agencia;
-    saldo;
+    _saldo = 0;
 }
 
 const cliente1 = new Cliente();
 
 cliente1.nome = 'Ricardo';
 cliente1.cpf = 64062389070;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
 const cliente2 = new Cliente();
 cliente2.nome = 'Alice';
 cliente2.cpf = 54717228090;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1, cliente2);
+console.log(cliente1);
+console.log(cliente2);
